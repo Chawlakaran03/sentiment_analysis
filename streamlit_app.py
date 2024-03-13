cf8e3c8b40287ed1d4b7167762d6e1e315e15162
Karan Chawla <135589941+Chawlakaran03@users.noreply.github.com>
2024-03-13 17:31:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,6 @@ subprocess.check_call([sys.executable, "-m", "pip", "install", "textblob"])
 from textblob import TextBlob
 import pandas as pd
 import streamlit as st
-import cleantext
-
 
 st.header('Sentiment Analysis')
 with st.expander('Analyze Text'):
@@ -16,12 +14,6 @@ with st.expander('Analyze Text'):
         blob = TextBlob(text)
         st.write('Polarity: ', round(blob.sentiment.polarity,2))
         st.write('Subjectivity: ', round(blob.sentiment.subjectivity,2))
-
-
-    pre = st.text_input('Clean Text: ')
-    if pre:
-        st.write(cleantext.clean(pre, clean_all= False, extra_spaces=True ,
-                                 stopwords=True ,lowercase=True ,numbers=True , punct=True))
 
 with st.expander('Analyze CSV'):
     upl = st.file_uploader('Upload file')
